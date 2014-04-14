4fcb23b787a33266b5113781e8653e7e0dff27d7
Emil Palm <emil@x86.nu>
2014-04-14 17:15:25 +0200

    Moving around tests and adding imports to split out some fake classes

--- TPFactory/src/TPSubclassFactory.m
+++ TPFactory/src/TPSubclassFactory.m
@@ -7,7 +7,59 @@
 //
 
 #import "TPSubclassFactory.h"
+#import "TPBaseFactory+Private.h"
+
+@interface TPSubclassFactory () {
+    NSSet *_classes;
+    Class parentClass;
+}
+@end
 
 @implementation TPSubclassFactory
+- (id)initWithClass:(Class)parentClass {
+    
+    self = [self init];
+    @synchronized(self) {
+        if ( self ) {
+            protocol = objc_getProtocol("TPBaseFactoryProtocol");
+        }
+    }
+    return self;
+}
+
+- (NSSet *) classes {
+    if ( !_classes ) {
+        NSUInteger numClasses = objc_getClassList(NULL, 0);
+        if (numClasses > 0 )
+        {
+            NSMutableSet *classesConforming = [NSMutableSet setWithCapacity:numClasses];
+            Class *classes = (__unsafe_unretained Class *)malloc(sizeof(Class) * numClasses);
+            if ( classes != NULL) {
+                numClasses = objc_getClassList(classes, (int)numClasses);
+                for (int i = 0; i < numClasses; i++) {
+                    if ( class_getSuperclass(classes[i]) == parentClass ) {
+                        Class cls = classes[i];
+                        if ( class_conformsToProtocol(cls, protocol) ) {
+                            [classesConforming setByAddingObject:cls];
+                        }
+                    }
+                }
+                _classes = [classesConforming copy];
+            }
+        }
+    }
+    return _classes;
+}
+
+- (Class)classForObject:(id<NSObject>)obj {
+    for (Class<TPBaseFactoryProtocol> cls in [self _classes]) {
+        if ( class_respondsToSelector(cls, @selector(canHandleObject:)) ) {
+            if ( [cls canHandleObject: obj] ) {
+                return cls;
+            }
+        }
+    }
+    return nil;
+}
 
 @end
